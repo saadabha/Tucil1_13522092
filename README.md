b514ff60efe19556bc54b352eec89683ba8f86f5
saadabha <saadabdulhakim08@gmail.com>
2024-02-13 01:25:09 +0700

    feat: add laporan, main.exe, update readme and main.py

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-# Tucil1_13522092
-Tucil Stima
+# TUGAS KECIL 1 IF2211 Strategi Algoritma
+Program Penyelesaian Cyberpunk 2077 Breach Protocol dengan Algoritma Brute Force
+
+Program ini menggunakan algoritma Brute Force dalam mencari semua kemungkinan/kombinasi buffer untuk menyelesaikan mini game dari permainan video Cyberpunk 2077
+
+Cara Menjalankan dan Menggunakan Program
+1. Siapkan file di dalam folder test yang berisi ukuran buffer, lebar matriks, tinggi matriks, matriks, banyaknya sekuens, sekuens dan rewardnya jika ingin menggunakan input berupa file
+2. Jalankan program main.exe
+3. Pilih input yang diinginkan
+4. Masukkan nama file jika memilih input berupa file atau masukkan jumlah token unik, token, ukuran buffer, ukuran matriks, jumlah sekuens, dan ukuran maksimal sekuens ke dalam terminal jika memilih input secara manual
+5. Setelah program berhasil dijalankan masukkan "y" jika ingin menyimpan hasil output program ke dalam file dan masukkan "n" jika tidak
+6. Masukkan nama file untuk menyimpan hasil output program jika memilih untuk menyimpan
+7. Hasil output program akan tersimpan pada folder test
 
 Sa'ad Abdul Hakim
 13522092
--- src/main.py
+++ src/main.py
@@ -1,6 +1,25 @@
 import random
+import time
 
 
+GREEN = "\033[32m"
+END = "\033[0m"
+print(f"""{GREEN}     
+ _____         _                                       _         _____  _____  ______ ______
+/  __ \       | |                                     | |       / __  \|  _  ||___  /|___  /
+| /  \/ _   _ | |__    ___  _ __  _ __   _   _  _ __  | | __    `' / /'| |/' |   / /    / / 
+| |    | | | || '_ \  / _ \| '__|| '_ \ | | | || '_ \ | |/ /      / /  |  /| |  / /    / /  
+| \__/\| |_| || |_) ||  __/| |   | |_) || |_| || | | ||   <     ./ /___\ |_/ /./ /   ./ /   
+ \____/ \__, ||_.__/  \___||_|   | .__/  \__,_||_| |_||_|\_\    \_____/ \___/ \_/    \_/    
+         __/ |                   | |                                                        
+        |___/                    |_|                                                        
+______                           _         ______               _                        _  
+| ___ \                         | |        | ___ \             | |                      | | 
+| |_/ / _ __   ___   __ _   ___ | |__      | |_/ / _ __   ___  | |_   ___    ___   ___  | | 
+| ___ \| '__| / _ \ / _` | / __|| '_ \     |  __/ | '__| / _ \ | __| / _ \  / __| / _ \ | | 
+| |_/ /| |   |  __/| (_| || (__ | | | |    | |    | |   | (_) || |_ | (_) || (__ | (_) || | 
+\____/ |_|    \___| \__,_| \___||_| |_|    \_|    |_|    \___/  \__| \___/  \___| \___/ |_|                                                                                                                                                                                         
+{END}""")
 print("Selamat datang dalam permainan Breach Protocol")
 print("Silahkan pilih masukan yang anda inginkan")
 print("1. File .txt")
@@ -14,7 +33,7 @@ while pilih != 1 and pilih != 2:
 
 if pilih == 1:
     filename = input("Masukkan nama file: ")
-    file = open('load.txt','r')
+    file = open('../test/'+filename,'r')
     buffer_length = int(file.readline())
     content = file.readline()
     content = content.split()
@@ -59,10 +78,29 @@ else:
         sequence = []
         for j in range(length):
             sequence.append(random.choice(token))
+        if i > 0:
+            for i in range(len(sequence_list)):
+                listcheck = sequence_list[i][1]
+                while listcheck == sequence:
+                    sequence.pop()
+                    sequence.append(random.choice(token))
         sequence_reward = random.randint(-100, 100)
         sequence_list.append((sequence_reward, sequence))
-
-file = open('tes.txt','w')
+    print()
+    print("Matrix")
+    for row in matrix:
+        for element in row:
+            print(element, end=" ")
+        print()
+    print()
+    print("Sequence")
+    for i in range(len(sequence_list)):
+        sekuens = sequence_list[i][1]
+        for element in sekuens:
+            print(element, end=" ")
+        print()
+        print(sequence_list[i][0])
+    print()
 
 def reward_count(sequence, buffer):
     reward = 0
@@ -81,17 +119,31 @@ def is_sublist(sub_list, list):
 def is_elmt(sublist, koordinat):
     return sublist in koordinat
 
+def reward_check(rwrd, buff, koord):
+    global reward, buffer, koordinat
+    if rwrd >= reward:
+        if rwrd == reward:
+            if len(buff) < len(buffer):
+                reward = rwrd
+                buffer = buff.copy()
+                koordinat = koord.copy()
+        else:
+            reward = rwrd
+            buffer = buff.copy()
+            koordinat = koord.copy()
+    
+
 def add_buffer(currbuffer, koor, bool, templength):
     if bool:
         i = koor[len(koor)-1][0]
-        for j in range(mheight):
+        for j in range(mwidth):
             if j != koor[len(koor)-1][1] and not is_elmt([i, j], koor):
                 currbuffer.append(matrix[i][j])
                 koor.append([i, j])
                 templength += 1
                 bool = False
                 reward = reward_count(sequence_list, currbuffer)
-                file.write(str(reward)+' '+ str(currbuffer) + ' '+"\n")
+                reward_check(reward, currbuffer, koor)
                 if templength < buffer_length:
                     add_buffer(currbuffer, koor, bool, templength)
                 currbuffer.pop()
@@ -99,14 +151,14 @@ def add_buffer(currbuffer, koor, bool, templength):
                 templength -= 1
     else:
         j = koor[len(koor)-1][1]
-        for i in range(mwidth):
+        for i in range(mheight):
             if i != koor[len(koor)-1][0] and not is_elmt([i, j], koor):
                 currbuffer.append(matrix[i][j])
                 koor.append([i, j])
                 templength += 1
                 bool = True
                 reward = reward_count(sequence_list, currbuffer)
-                file.write(str(reward)+' '+ str(currbuffer) + ' '+"\n")
+                reward_check(reward, currbuffer, koor)
                 if templength < buffer_length:
                     add_buffer(currbuffer, koor, bool, templength)
                 currbuffer.pop()
@@ -116,6 +168,8 @@ def add_buffer(currbuffer, koor, bool, templength):
 reward = 0
 buffer = []
 koordinat = []
+start = time.time()
+
 if buffer_length != 0:
     for i in range(mwidth):
         buffertemp = []
@@ -124,8 +178,53 @@ if buffer_length != 0:
         buffertemp.append(matrix[0][i])
         koordinattemp.append([0, i])
         templength += 1
-        reward = reward_count(sequence_list, buffertemp)
-        file.write(str(reward)+' '+ str(buffertemp) + ' '+"\n")
+        rewardtemp = reward_count(sequence_list, buffertemp)
+        if i == 0:
+            reward = rewardtemp
+            buffer = buffertemp.copy()
+            koordinat = koordinattemp.copy()
+        else:
+            reward_check(rewardtemp, buffertemp, koordinattemp)
         if buffer_length > 1:
             add_buffer(buffertemp, koordinattemp, False, templength)
+    print(reward)
+    for element in buffer:
+        print(element, end=' ')
+    print()
+    for i in range(len(koordinat)):
+        print(koordinat[i][1]+1, end=", ")
+        print(koordinat[i][0]+1)
+    print()
+else:
+    print("Tidak ada solusi")
+    print()
+
+end = time.time()
+print((end-start)*1000, end=" ms")
+print()
+save = input("Apakah ingin menyimpan solusi? (y/n) ")
+save = save.lower()
+
+if save == "y":
+    filename = input("Masukkan nama file: ")
+    file = open("../test/"+filename,"w")
+    if buffer_length == 0:
+        file.write("Tidak ada solusi\n")
+    else:
+        file.write(str(reward))
+        file.write("\n")
+        for element in buffer:
+            file.write(str(element))
+            file.write(" ")
+        file.write("\n")
+        for i in range(len(koordinat)):
+            file.write(str(koordinat[i][1]+1))
+            file.write(", ")
+            file.write(str(koordinat[i][0]+1))
+            file.write("\n")
+        file.write("\n")
+    file.write(str((end-start)*1000))
+    file.write(" ms\n")    
+    file.close()
+
     
